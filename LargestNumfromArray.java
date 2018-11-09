a9bd0d309e8f3c9dde29470a8f6002e36da7c709
SumiMajumder <44892551+SumiMajumder@users.noreply.github.com>
2018-11-09 14:03:56 +0530

    Add files via upload
    Updated one

--- LargestNumfromArray.java
+++ LargestNumfromArray.java
@@ -19,22 +19,20 @@ public class LargestNumfromArray {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		TestANotherClass obj = new TestANotherClass();
-//		 obj.LarCheck();
-//		 obj.PrintHello();
-		int[] arr = { 1, 6, 4, 3, 7, 49,9 };
-//		@SuppressWarnings("resource")
-//		Scanner in = new Scanner(System.in);
-//		System.out.println("Please enter the value from array which you want to search-");
-//		int a = in.nextInt();
-//		boolean result = obj.arrayExistInCheck(arr, a);
-//		if (result == true) {
-//			System.out.println("Array found");
-//		} else {
-//			System.out.println("Not found");
-//		}
-	int[] reversearray = obj.arrayrReverseWithoutarray(arr);
-	System.out.println("Array reverse " +Arrays.toString(reversearray));
+		 obj.LarCheck();
+		 obj.PrintHello();
+		int[] arr = { 1, 6, 4, 3, 7, 49, 9 };
+		@SuppressWarnings("resource")
+		Scanner in = new Scanner(System.in);
+		System.out.println("Please enter the value from array which you want to search-");
+		int a = in.nextInt();
+		boolean result = obj.arrayExistInCheck(arr, a);
+		if (result == true) {
+			System.out.println("Array found");
+		} else {
+			System.out.println("Not found");
+		}
+		int[] reversearray = obj.arrayrReverseWithoutarray(arr);
+		System.out.println("Array reverse " + Arrays.toString(reversearray));
 	}
-//		obj.arrayrReverseWithoutarray(arr);
-//	}
 }
